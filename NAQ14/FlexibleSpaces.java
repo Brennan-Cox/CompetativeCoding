8f3f7f270d4396432ef233dd7e31016c69f8747a
Brennan-Cox <bhcox1@radford.edu>
2023-11-07 16:14:21 -0500

    yes

--- NAQ14/FlexibleSpaces.java
+++ NAQ14/FlexibleSpaces.java
@@ -3,10 +3,38 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class FlexibleSpaces {
     FlexibleSpaces() {
+        FastScanner fs = new FastScanner(System.in);
+        int w = fs.nextInt();
+        int[] partitions = new int[fs.nextInt() + 2];
+        {
+            int index = 0;
+            partitions[index++] = 0;
+            for (;index < partitions.length - 1; index++) {
+                partitions[index] = fs.nextInt();
+            }
+            partitions[index] = w;
+        }
+        // Guarantees no duplicates
+        Set<Integer> possibleWidths = new HashSet<>();
+        for (int i = 0; i < partitions.length; i++) {
+            for (int j = i + 1; j < partitions.length; j++) {
+                possibleWidths.add(partitions[j] - partitions[i]);
+            }
+        }
+        // Guarantees sorted
+        List<Integer> list = new ArrayList<>(possibleWidths);
+        list.sort((o1, o2) -> o1 - o2);
+        StringBuilder sb = new StringBuilder();
+        list.stream().forEach((n) -> sb.append(n + " "));
+        System.out.println(sb.toString().trim());
     }
     public static void main(String[] args) {
         new FlexibleSpaces();
